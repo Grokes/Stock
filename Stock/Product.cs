3f0833d6349280221f67932591193fbabe239cbb
Grokes <default1one@gmail.com>
2023-09-09 12:17:28 +0300

    Add project files.

---
+++ Stock/Product.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stock
+{
+    class Product
+    {
+        int id;
+        string number;
+        string name;
+        double cost;
+        int count;
+        public int getid() { return id; }
+        public string getnum() { return number; }
+        public string getname() { return name; }
+        public double getcost() { return cost; }
+        public int getcount() { return count; }
+        public Product(int id, string number = "000000", string name = "default", double cost = 0, int count = 0)
+        {
+            if (number.Length == 6 && cost >= 0)
+            {
+                this.id = id;
+                this.number = number;
+                this.name = name;
+                this.cost = cost;
+                this.count = count;
+            }
+        }
+        public string show()
+        {
+            return id + " " + number + " " + name + " " + cost + " " + count;
+        }
+    }
+    class Base
+    {
+        Product[] products = new Product[10];
+        int size = 0;
+        int capacity = 10;
+        public void add(string number = "000000", string name = "default", int cost = 0, int count = 0)
+        {
+            if (size == capacity - 1)
+                upsize();
+            products[size] = new Product(size, number, name, cost, count);
+            ++size;
+        }
+        public string select()
+        {
+            string result = "";
+            for (int i = 0; i < size; ++i)
+            {
+                result += products[i].show() + "\n";
+            }
+            return result;
+        }
+        private void upsize()
+        {
+            capacity *= 2;
+            Product[] temp = new Product[capacity];
+            products.CopyTo(temp, 0);
+            this.products = temp;
+        }
+    }
+}
---
+++ Stock/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stock
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Base stock = new Base();
+            while (true)
+            {
+                string command = Console.ReadLine();
+                Console.WriteLine(determineCommand(command, stock));
+            }
+
+        }
+        static public string determineCommand(string command, Base stock)
+        {
+            if (command.StartsWith("ADD"))
+            {
+                command = command.Substring(command.IndexOf('(') + 1, command.Length - 6);
+                string[] arrCommand = command.Split(',');
+                stock.add(arrCommand[0], arrCommand[1], int.Parse(arrCommand[2]), int.Parse(arrCommand[3]));
+                return "Строка добавлена";
+            }
+            if ((command.StartsWith("SELECT")))
+            {
+                return stock.select();
+            }
+            return "Ошибка в ключевом слове.";
+
+        }
+    }
+}
